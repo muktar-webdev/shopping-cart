d5be8640516c095ba901e63f52d3a0889ea639e5
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-01-27 01:10:06 +0600

    s

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ shoppingCartPlusIcon.addEventListener("click",function(){
     // console.log(shoppingCartProductPriceUpdate);
     document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
 })
+
+// --Shopping Cart Minus Icon Event Handler--//
+const shoppingCartMinusIcon = document.getElementById("shoppingCartMinusIcon");
+shoppingCartMinusIcon.addEventListener("click",function(){
+    // --Shopping Cart Value--//
+    const shoppingCartValue = document.getElementById("shoppingCartValue").value;
+    const shoppingCartValueNumber = parseInt(shoppingCartValue);
+    const shoppingCartValueUpdate = shoppingCartValueNumber -1;
+    document.getElementById("shoppingCartValue").value = shoppingCartValueUpdate;
+
+    // --Shopping Cart Product Price--//
+    const shoppingCartProductPrice = document.getElementById("shoppingCartProductPrice").innerText;
+    const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
+    const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber - 1219;
+    // console.log(shoppingCartProductPriceUpdate);
+    document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
+})
