d675f2b65f5f06b67aa6ca6998a64cb4826a3de6
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-01-27 02:15:13 +0600

    Shopping Cart

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // --Shopping Cart Plus Icon Event Handler--//
 const shoppingCartPlusIcon = document.getElementById("shoppingCartPlusIcon");
-shoppingCartPlusIcon.addEventListener("click",function(){
+    shoppingCartPlusIcon.addEventListener("click",function(){
     // --Shopping Cart Value--//
     const shoppingCartValue = document.getElementById("shoppingCartValue").value;
     const shoppingCartValueNumber = parseInt(shoppingCartValue);
@@ -13,11 +13,24 @@ shoppingCartPlusIcon.addEventListener("click",function(){
     const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber + 1219;
     // console.log(shoppingCartProductPriceUpdate);
     document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
+
+    // --Subtotal--//
+    const subTotal = document.getElementById("subtotal").innerText;
+    const subTotalNumber = parseInt(subTotal);
+    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
+    document.getElementById("subtotal").innerText = subTotalNumberUpdate;
+
+    // --Tax & total--//
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax);
+    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+    document.getElementById("total").innerText = taxNumberUpdate;
+
 })
 
 // --Shopping Cart Minus Icon Event Handler--//
 const shoppingCartMinusIcon = document.getElementById("shoppingCartMinusIcon");
-shoppingCartMinusIcon.addEventListener("click",function(){
+    shoppingCartMinusIcon.addEventListener("click",function(){
     // --Shopping Cart Value--//
     const shoppingCartValue = document.getElementById("shoppingCartValue").value;
     const shoppingCartValueNumber = parseInt(shoppingCartValue);
@@ -30,4 +43,22 @@ shoppingCartMinusIcon.addEventListener("click",function(){
     const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber - 1219;
     // console.log(shoppingCartProductPriceUpdate);
     document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
+
+    // --Subtotal--//
+    const subTotal = document.getElementById("subtotal").innerText;
+    const subTotalNumber = parseInt(subTotal);
+    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
+    document.getElementById("subtotal").innerText = subTotalNumberUpdate;
+
+    // --Tax & total--//
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax);
+    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+    document.getElementById("total").innerText = taxNumberUpdate;
+    // document.getElementById("total").innerText = 0;
 })
+// --First Product End--//
+
+// --Second Product Start Here--//
+// --Second Product End Here--//
+
