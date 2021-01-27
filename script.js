75b48c25b7bd9aec1aa9cbe62b2877c2435746c2
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-01-27 13:26:19 +0600

    shopping cart part-2

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-// --Shopping Cart Plus Icon Event Handler--//
+// --Iphone 11--//
+// --Shopping Cart Plus Icon Event Handler-- //
 const shoppingCartPlusIcon = document.getElementById("shoppingCartPlusIcon");
-    shoppingCartPlusIcon.addEventListener("click",function(){
-    // --Shopping Cart Value--//
-    const shoppingCartValue = document.getElementById("shoppingCartValue").value;
-    const shoppingCartValueNumber = parseInt(shoppingCartValue);
-    const shoppingCartValueUpdate = shoppingCartValueNumber + 1;
-    document.getElementById("shoppingCartValue").value = shoppingCartValueUpdate;
-
-    // --Shopping Cart Product Price--//
-    const shoppingCartProductPrice = document.getElementById("shoppingCartProductPrice").innerText;
-    const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
-    const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber + 1219;
-    // console.log(shoppingCartProductPriceUpdate);
-    document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
-
-    // --Subtotal--//
-    const subTotal = document.getElementById("subtotal").innerText;
-    const subTotalNumber = parseInt(subTotal);
-    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
-    document.getElementById("subtotal").innerText = subTotalNumberUpdate;
-
-    // --Tax & total--//
-    const tax = document.getElementById("tax").innerText;
-    const taxNumber = parseInt(tax);
-    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
-    document.getElementById("total").innerText = taxNumberUpdate;
+shoppingCartPlusIcon.addEventListener("click", function() {
+    updateSpanText("shoppingCartValue", "shoppingCartProductPrice", "subtotal", "tax", "total");
 
 })
 
 // --Shopping Cart Minus Icon Event Handler--//
 const shoppingCartMinusIcon = document.getElementById("shoppingCartMinusIcon");
-    shoppingCartMinusIcon.addEventListener("click",function(){
+shoppingCartMinusIcon.addEventListener("click", function() {
+
     // --Shopping Cart Value--//
     const shoppingCartValue = document.getElementById("shoppingCartValue").value;
     const shoppingCartValueNumber = parseInt(shoppingCartValue);
-    const shoppingCartValueUpdate = shoppingCartValueNumber -1;
+    const shoppingCartValueUpdate = shoppingCartValueNumber - 1;
     document.getElementById("shoppingCartValue").value = shoppingCartValueUpdate;
 
     // --Shopping Cart Product Price--//
     const shoppingCartProductPrice = document.getElementById("shoppingCartProductPrice").innerText;
     const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
     const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber - 1219;
-    // console.log(shoppingCartProductPriceUpdate);
     document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
 
     // --Subtotal--//
@@ -55,10 +33,61 @@ const shoppingCartMinusIcon = document.getElementById("shoppingCartMinusIcon");
     const taxNumber = parseInt(tax);
     const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
     document.getElementById("total").innerText = taxNumberUpdate;
-    // document.getElementById("total").innerText = 0;
 })
-// --First Product End--//
 
-// --Second Product Start Here--//
-// --Second Product End Here--//
+// --Common Function for Plus & Minus Icon--//
+function updateSpanText(id1, id2, id3, id4, id5) {
+    // --Shopping Cart Value--//
+    const shoppingCartValue = document.getElementById(id1).value;
+    const shoppingCartValueNumber = parseInt(shoppingCartValue);
+    const shoppingCartValueUpdate = shoppingCartValueNumber + 1;
+    document.getElementById(id1).value = shoppingCartValueUpdate;
 
+    // --Shopping Cart Product Price--//
+    const shoppingCartProductPrice = document.getElementById(id2).innerText;
+    const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
+    const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber + 1219;
+    document.getElementById(id2).innerText = shoppingCartProductPriceUpdate;
+
+    // --Subtotal--//
+    const subTotal = document.getElementById(id3).innerText;
+    const subTotalNumber = parseInt(subTotal);
+    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
+    document.getElementById(id3).innerText = subTotalNumberUpdate;
+
+    // --Tax & total--//
+    const tax = document.getElementById(id4).innerText;
+    const taxNumber = parseInt(tax);
+    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+    document.getElementById(id5).innerText = taxNumberUpdate;
+}
+
+
+// --Iphone 11 Case---//
+
+// --Plus Event Handler--//
+const plusIcon = document.getElementById("plusIcon");
+plusIcon.addEventListener("click", function() {
+        // --Shopping Cart Value--//
+    const value = document.getElementById("value").value;
+    const valueNumber = parseInt(value);
+    const valueUpdate = valueNumber + 1;
+    document.getElementById("value").value = valueUpdate;
+    // --Shopping Cart Product Price--//
+    const casePrice = document.getElementById("iphoneCasePrice").innerText;
+    const casePriceNumber = parseInt(casePrice);
+    const casePriceUpdate = casePriceNumber + 59;
+    document.getElementById("iphoneCasePrice").innerText = casePriceUpdate;
+
+     // --Subtotal--//
+     const subTotal = document.getElementById("subtotal").innerText;
+     const subTotalNumber = parseInt(subTotal);
+     const subTotalNumberUpdate = casePriceUpdate;
+     document.getElementById("subtotal").innerText = subTotalNumberUpdate;
+ 
+     // --Tax & total--//
+     const tax = document.getElementById("tax").innerText;
+     const taxNumber = parseInt(tax);
+     const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+     document.getElementById("total").innerText = taxNumberUpdate;
+})
---
+++ withoutFunctionality.js
@@ -0,0 +1,58 @@
+// --Shopping Cart Plus Icon Event Handler--//
+
+const shoppingCartPlusIcon = document.getElementById("shoppingCartPlusIcon");
+shoppingCartPlusIcon.addEventListener("click", function() {
+    updateSpanText("shoppingCartValue", "shoppingCartProductPrice", "subtotal", "tax", "total")
+        // --Shopping Cart Value--//
+    const shoppingCartValue = document.getElementById("shoppingCartValue").value;
+    const shoppingCartValueNumber = parseInt(shoppingCartValue);
+    const shoppingCartValueUpdate = shoppingCartValueNumber + 1;
+    document.getElementById("shoppingCartValue").value = shoppingCartValueUpdate;
+
+    // --Shopping Cart Product Price--//
+    const shoppingCartProductPrice = document.getElementById("shoppingCartProductPrice").innerText;
+    const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
+    const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber + 1219;
+    document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
+
+    // --Subtotal--//
+    const subTotal = document.getElementById("subtotal").innerText;
+    const subTotalNumber = parseInt(subTotal);
+    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
+    document.getElementById("subtotal").innerText = subTotalNumberUpdate;
+
+    // --Tax & total--//
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax);
+    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+    document.getElementById("total").innerText = taxNumberUpdate;
+
+})
+
+// --Shopping Cart Minus Icon Event Handler--//
+const shoppingCartMinusIcon = document.getElementById("shoppingCartMinusIcon");
+shoppingCartMinusIcon.addEventListener("click", function() {
+    // --Shopping Cart Value--//
+    const shoppingCartValue = document.getElementById("shoppingCartValue").value;
+    const shoppingCartValueNumber = parseInt(shoppingCartValue);
+    const shoppingCartValueUpdate = shoppingCartValueNumber - 1;
+    document.getElementById("shoppingCartValue").value = shoppingCartValueUpdate;
+
+    // --Shopping Cart Product Price--//
+    const shoppingCartProductPrice = document.getElementById("shoppingCartProductPrice").innerText;
+    const shoppingCartProductPriceNumber = parseInt(shoppingCartProductPrice);
+    const shoppingCartProductPriceUpdate = shoppingCartProductPriceNumber - 1219;
+    document.getElementById("shoppingCartProductPrice").innerText = shoppingCartProductPriceUpdate;
+
+    // --Subtotal--//
+    const subTotal = document.getElementById("subtotal").innerText;
+    const subTotalNumber = parseInt(subTotal);
+    const subTotalNumberUpdate = shoppingCartProductPriceUpdate;
+    document.getElementById("subtotal").innerText = subTotalNumberUpdate;
+
+    // --Tax & total--//
+    const tax = document.getElementById("tax").innerText;
+    const taxNumber = parseInt(tax);
+    const taxNumberUpdate = subTotalNumberUpdate + taxNumber;
+    document.getElementById("total").innerText = taxNumberUpdate;
+})
